0972f2339ef2d7f320cdbfe08fabc005d0296683
Fehmi Baltaci <baltacifehmi@gmail.com>
2024-11-18 16:23:38 -0500

    Fixed the loop for products page.

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -10,5 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
-  @Input() data!: IProductDetailsResponse[];
+  @Input() productDetail!: IProductDetailsResponse;
 }
