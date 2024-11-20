3d2d0df63f4a3828852dd7d933a25113fe2e7536
Fehmi Baltaci <baltacifehmi@gmail.com>
2024-11-20 15:03:52 -0500

    footer component is added to all pages now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { FooterComponent } from './components/footer/footer.component';
 
 /**
  * AppComponent
@@ -10,7 +11,13 @@ import { MatDialogModule } from '@angular/material/dialog';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, CommonModule, HeaderComponent, MatDialogModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    MatDialogModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
